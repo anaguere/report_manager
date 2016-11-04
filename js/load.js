9fedeba80958922f62b945db33e7e52b696d8794
root <root@pitagora.pitgora>
2016-11-04 03:13:29 -0400

    cambios css load.html

--- js/load.js
+++ js/load.js
@@ -66,9 +66,9 @@
                }).done(function(data){
 
 
-                 var news_det = JSON.parse(data);
+                   var news_det = JSON.parse(data);
 
-                 $.each(news_det.contenido,function(i,n){
+                   $.each(news_det.contenido,function(i,n){
 
                     $("#news_title_es").val(n.news_det_tit);
                     $("#news_date").val(n.news_det_date);
@@ -78,55 +78,59 @@
                     $("#news_source").val(n.news_det_source);
                 //    $("#news_title_es").val(n.news_det_category);
                 //    $("#news_title_es").val(n.news_det_image);
-                    $("#news_title_en").val(n.news_det_tit_en);
-                    $("#news_comment_en").val(n.news_det_text_en);
-                    $("#news_title_es").val(n.news_det_text);
-
-                    console.log(n);
-
-                         $.each(n.news_det_category,function(x,z){
- 
-                            $(".div_news_category :input ").val( z.news_cat_name );        
+                $("#news_title_en").val(n.news_det_tit_en);
+                $("#news_comment_en").val(n.news_det_text_en);
+                $("#news_title_es").val(n.news_det_text);
 
 
-                         });
+                $.each(n.news_det_category,function(x,z){
+                   
+                    $(".div_news_category :input ").val( z.news_cat_name );        
+
 
                 });
 
-
-             });
-           }
+            });
 
 
+               });
+
+        
+        $('.button-collapse').sideNav('hide');
 
 
-function preview(){
-    $('#news_body_es').focusout(function(){
-        var news_body_html = $(this).html();
-        var news_img = $(news_body_html).contents().find("img").attr("src");
-        if (news_img != null) {
-            $('#h5_title').after("<img id='new_img_prev' src='"+news_img+"' style='margin-left: 5%;' width='90%' height='35%'> ");
-        }else{
-            $('#h5_title').after("<hr></hr>");
-        }
-        $('#new_img_prev').attr('src',news_img);
-        $('#news_body_prev').text($(this).text());
-    });
-    $('#news_title_es').focusout(function(){
-        $('#news_title_prev').text($(this).val());
-    });
-    $('#news_date').change(function(){
-        var d = new Date($('#news_date').val());
-        var date = $('#news_date').val();
-        date = date.split(" ");
-        $('#news_month_prev').text(date[1]+date[2]+".-");
-        $('#news_date_prev').text(d.getDate()+"/"+d.getMonth()+"/"+d.getFullYear());
-    });
-    $('#news_source').focusout(function(){
-        $('#news_source_prev').text($(this).val());
-    });
+    }
 
-}
+
+
+
+    function preview(){
+        $('#news_body_es').focusout(function(){
+            var news_body_html = $(this).html();
+            var news_img = $(news_body_html).contents().find("img").attr("src");
+            if (news_img != null) {
+                $('#h5_title').after("<img id='new_img_prev' src='"+news_img+"' style='margin-left: 5%;' width='90%' height='35%'> ");
+            }else{
+                $('#h5_title').after("<hr></hr>");
+            }
+            $('#new_img_prev').attr('src',news_img);
+            $('#news_body_prev').text($(this).text());
+        });
+        $('#news_title_es').focusout(function(){
+            $('#news_title_prev').text($(this).val());
+        });
+        $('#news_date').change(function(){
+            var d = new Date($('#news_date').val());
+            var date = $('#news_date').val();
+            date = date.split(" ");
+            $('#news_month_prev').text(date[1]+date[2]+".-");
+            $('#news_date_prev').text(d.getDate()+"/"+d.getMonth()+"/"+d.getFullYear());
+        });
+        $('#news_source').focusout(function(){
+            $('#news_source_prev').text($(this).val());
+        });
+
+    }
         //Sending news to database
         function SaveNews(){
             var news_body_html = $('#news_body_es').html();
