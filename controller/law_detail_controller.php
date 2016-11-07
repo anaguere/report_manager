19daaaef32d601a4b2a4c6c67a91c98ea2eb2f3e
annralf <anaguere@gmail.com>
2016-11-06 21:42:36 -0500

    redireccion al index

--- controller/law_detail_controller.php
+++ controller/law_detail_controller.php
@@ -1,4 +1,7 @@
 <?php
+header( 'Cache-Control: no-store, no-cache, must-revalidate' );
+header( 'Cache-Control: post-check=0, pre-check=0', false );
+header( 'Pragma: no-cache' );
 require '../libraries/get_contents.php';
 require '../model/connector.php';
 require '../model/law_detail.php';
